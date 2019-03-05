a42defe4261222c8fe14130a1c008c008ee26f81
Choffs <chris932100@gmail.com>
2019-03-05 15:11:47 -0500

    Basic Layout Started/Bootstrap added.

---
+++ src/app/menu-list/menu-object/menu-object.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-object',
+  templateUrl: './menu-object.component.html',
+  styleUrls: ['./menu-object.component.css']
+})
+export class MenuObjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
